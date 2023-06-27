7fb39e2c3300b7d1bdb9b26dbf73934edf4d29cc
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-06-27 21:47:44 +0700

    Update imagebuilderamlogicservice.sh

--- config-openwrt/arf-wrt/imagebuilderamlogicservice.sh
+++ config-openwrt/arf-wrt/imagebuilderamlogicservice.sh
@@ -178,11 +178,11 @@ rebuild_firmware() {
     # sorting by https://build.moz.one
     my_packages="\
         acpid attr base-files bash bc blkid block-mount blockd bsdtar \
-        btrfs-progs busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark \
+        busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark \
         coreutils coreutils-base64 coreutils-nohup coreutils-truncate curl \
         dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs \
         f2fs-tools f2fsck fdisk gawk getopt gzip hostapd-common iconv iw iwinfo jq jshn \
-        kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script \
+        kmod-brcmfmac kmod-brcmutil libjson-script \
         liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs \
         mount-utils openssl-util parted perl-http-date perlbase-file perlbase-getopt \
         perlbase-time perlbase-unicode perlbase-utf8 pigz ppp ppp-mod-pppoe \
@@ -191,17 +191,24 @@ rebuild_firmware() {
         uuidgen wget-ssl whereis which wpad-basic wwan xfs-fsck xfs-mkfs xz \
         xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
         \
+        libiwinfo libiwinfo-data libiwinfo-lua liblua liblucihttp-lua \
+        libubus-lua luci-app-firewall luci-app-opkg luci-ssl \
+        px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci \
+        rpcd-mod-rrdns adb \
+        \
         luci luci-base luci-compat luci-i18n-base-en luci-i18n-base-zh-cn luci-lib-base  \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
         luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
         \
-        kmod-usb-net-rndis kmod-usb-net-cdc-ncm kmod-usb-net-cdc-eem kmod-usb-net-cdc-ether \
+        kmod-usb-net-rndis kmod-usb-net-cdc-ncm kmod-usb-net-cdc-eem \
         kmod-usb-net-cdc-subset kmod-nls-base kmod-usb-core kmod-usb-net \
         kmod-usb-net-cdc-ether kmod-usb2 \
         \
-        adb \
+        ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common \
+        kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash \
+        kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant \
         \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
