bbc70f80725977a5c81adf52571c3be717587e0d
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-06-28 21:42:15 +0700

    Update imagebuilder.sh

--- config-openwrt/arf-wrt/imagebuilder.sh
+++ config-openwrt/arf-wrt/imagebuilder.sh
@@ -178,30 +178,44 @@ rebuild_firmware() {
     # sorting by https://build.moz.one
     my_packages="\
         acpid attr base-files bash bc blkid block-mount blockd bsdtar \
-        btrfs-progs busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark \
+        busybox bzip2 cgi-io chattr comgt comgt-ncm containerd coremark \
         coreutils coreutils-base64 coreutils-nohup coreutils-truncate curl \
         dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs \
         f2fs-tools f2fsck fdisk gawk getopt gzip hostapd-common iconv iw iwinfo jq jshn \
-        kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script \
-        liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs \
+        kmod-brcmfmac kmod-brcmutil libjson-script \
+		\
+        libnetwork losetup lsattr lsblk lscpu mkf2fs \
         mount-utils openssl-util parted perl-http-date perlbase-file perlbase-getopt \
         perlbase-time perlbase-unicode perlbase-utf8 pigz ppp ppp-mod-pppoe \
-        proto-bonding pv rename resize2fs runc subversion-client subversion-libs tar \
-        tini ttyd tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch \
-        uuidgen wget-ssl whereis which wpad-basic wwan xfs-fsck xfs-mkfs xz \
-        xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
+        pv rename resize2fs runc subversion-client subversion-libs tar \
+        tini ttyd tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip \
+        uuidgen wget-ssl whereis which wwan xfs-fsck xfs-mkfs xz \
+        xz-utils ziptool zstd \
         \
-        luci luci-base luci-compat luci-i18n-base-en luci-i18n-base-zh-cn luci-lib-base  \
+        libiwinfo libiwinfo-data libiwinfo-lua liblua \
+        libubus-lua luci-app-firewall \
+        px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo \
+        rpcd-mod-rrdns adb \
+        \
+        luci luci-base luci-compat luci-i18n-base-en \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
-        luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
-        luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
+        luci-mod-admin-full \
+        luci-proto-3g luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
         luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay  \
         \
-        kmod-usb-net-rndis kmod-usb-net-cdc-ncm kmod-usb-net-cdc-eem kmod-usb-net-cdc-ether \
+		libpcre2 zlib libxml2 libpthread libgcc1 zoneinfo-core libstdcpp6 libzip-openssl \
+		libzip-gnutls libzip-mbedtls libzip-nossl libopenssl1.1 libgnutls \
+		libnettle8 libgmp10 libatomic1 libmbedtls12 oniguruma5 zoneinfo-asia \
+		\
+        kmod-usb-net-rndis kmod-usb-net-cdc-ncm kmod-usb-net-cdc-eem \
         kmod-usb-net-cdc-subset kmod-nls-base kmod-usb-core kmod-usb-net \
         kmod-usb-net-cdc-ether kmod-usb2 \
         \
-        adb \
+        ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k \
+        kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash \
+        kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant \
+        \
+        luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
         ${config_list} \
         "
